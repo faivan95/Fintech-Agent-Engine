f54b50376ec05bfc7c96c1a9a05c20fab0583527
Farhan Abid Ivan <faivan93@gmail.com>
2026-04-22 10:17:46 +0600

    feat: added postgresql integration and database initialization script

---
+++ app/main.py
@@ -0,0 +1,7 @@
+import time
+
+if __name__ == "__main__":
+    print("FinTech Agent Engine is running...")
+    # This loop keeps the Docker container alive in the background
+    while True:
+        time.sleep(60)
